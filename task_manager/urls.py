4a88396cfcebd98d2f48466de7a6e4681d073cca
rivrxsq <shevchenkovika01@gmail.com>
2024-11-07 11:26:44 +0200

    Add path for lists page for worker, task, tasktype and position in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import index, WorkerListView, TaskListView, TaskTypeListView, PositionListView
 
 urlpatterns = [
     path("", index, name="index"),
-    ]
+    path(
+        "workers/",
+        WorkerListView.as_view(),
+        name="worker-list",
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list",
+    ),
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list",
+    ),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list",
+    ),
+]
