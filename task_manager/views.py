bf6810024cb1a1ef1d8ab76b114dbf8e3a25dbbd
rivrxsq <shevchenkovika01@gmail.com>
2024-11-07 11:13:36 +0200

    Add list view for tasktype, task, worker and position

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from task_manager.models import (
     Position,
@@ -29,3 +30,80 @@ def index(request):
 
     return render(request, "task_manager/index.html", context=context)
 
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 5
+    queryset = Task.objects.select_related("task_type")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        context["search_form"] = TaskSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        model = self.request.GET.get("model")
+        if model:
+            return Task.objects.filter(model__icontains=model)
+        return Task.objects.all()
+
+
+class PositionListView(generic.ListView):
+    model = Position
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PositionListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        context["search_form"] = PositionSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            return Position.objects.filter(name__icontains=name)
+        return Position.objects.all()
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    paginate_by = 5
+    queryset = Worker.objects.select_related("position")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        context["search_form"] = WorkerSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            return Worker.objects.filter(name__icontains=name)
+        return Worker.objects.all()
+
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskTypeListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        context["search_form"] = TaskTypeSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            return TaskType.objects.filter(name__icontains=name)
+        return TaskType.objects.all()
