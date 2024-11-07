b4c8737cce3c4e587bf28a7da20dcf41c0586ba8
rivrxsq <shevchenkovika01@gmail.com>
2024-11-07 11:21:25 +0200

    Add search forms for tasktype, task, worker and position

---
+++ task_manager/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by task name"}
+        )
+    )
+
+class TaskTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by task type"}
+        )
+    )
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by worker name"}
+        )
+    )
+
+class PositionSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by position name"}
+        )
+    )
