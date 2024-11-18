5d489151bee17cdeb79744d5dee2f6b0d8ac5b73
rivrxsq <shevchenkovika01@gmail.com>
2024-11-19 00:58:39 +0200

    Add DATE_INPUT_FORMATS in settings.py

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATE_INPUT_FORMATS = ['%d-%m-%Y', '%Y-%m-%d']
