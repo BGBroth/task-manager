1f0cbf3b8d64450147e6e944d4fa8ab6b8cff5a5
rivrxsq <shevchenkovika01@gmail.com>
2024-12-04 23:51:51 +0200

    Add urls for projects, teams and tags in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,7 +21,22 @@ from task_manager.views import (
     PositionDetailView,
     PositionCreateView,
     PositionUpdateView,
-    PositionDeleteView
+    PositionDeleteView,
+    TagListView,
+    TagDetailView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TeamListView,
+    TeamDetailView,
+    TeamCreateView,
+    TeamUpdateView,
+    TeamDeleteView,
+    ProjectListView,
+    ProjectDetailView,
+    ProjectCreateView,
+    ProjectUpdateView,
+    ProjectDeleteView,
 )
 
 urlpatterns = [
@@ -46,6 +61,21 @@ urlpatterns = [
     path("positions/create/", PositionCreateView.as_view(), name="position-create"),
     path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
     path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("teams/", TeamListView.as_view(), name="team-list"),
+    path("teams/<int:pk>/", TeamDetailView.as_view(), name="team-detail"),
+    path("teams/create/", TeamCreateView.as_view(), name="team-create"),
+    path("teams/<int:pk>/update/", TeamUpdateView.as_view(), name="team-update"),
+    path("teams/<int:pk>/delete/", TeamDeleteView.as_view(), name="team-delete"),
+    path("projects/", ProjectListView.as_view(), name="project-list"),
+    path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("projects/create/", ProjectCreateView.as_view(), name="project-create"),
+    path("projects/<int:pk>/update/", ProjectUpdateView.as_view(), name="project-update"),
+    path("projects/<int:pk>/delete/", ProjectDeleteView.as_view(), name="project-delete"),
 ]
 
 app_name = "task_manager"
